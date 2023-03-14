9e5475d8954e4e23f8d14c7283461db656981d74
Micah Chiang <micahkchiang@gmail.com>
2023-03-14 16:00:59 -0500

    docusaurus initial...just realized committing to main, oops!

---
+++ ves-event-bus-docs/src/pages/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className={clsx(styles.container)}>
+        <h1 className={clsx(styles.title)}>Welcome to the Hedgehog Portal</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+          laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+      </div>
+      <div className={clsx(styles.hero__img)}>
+        <img src={require('@site/static/img/hedgehog_banner.png').default} />
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
