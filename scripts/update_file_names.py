7769b01474082af17681564ff13a34fb92269fec
Christine <yost90@gmail.com>
2024-07-03 16:24:37 -0600

    test new filename

--- scripts/update_file_names.py
+++ scripts/update_file_names.py
@@ -3,5 +3,6 @@ import os
 directory = 'wiki'
 
 for file in os.scandir(directory):
-    if file.is_file() and file.name.lower().endswith(('.md')):
-        print(file.name)
+    if file.is_file() and file.name.lower().endswith((".md")):
+        new_filename = file.name.replace("-", " ").title()
+        print(new_filename)
