d6e833d84fe0e531224e232d1285f84784d2f853
Christine <yost90@gmail.com>
2024-07-03 16:41:35 -0600

    test updating file names

--- scripts/update_file_names.py
+++ scripts/update_file_names.py
@@ -4,9 +4,10 @@ directory = 'wiki'
 
 for file in os.scandir(directory):
     if file.is_file() and file.name.lower().endswith('.md'):
+        filename = file.name
         split = file.name.split('-')
         capitalized = [string.capitalize() for string in split]
         new_filename = capitalized[0]
         for string in capitalized[1:]:
             new_filename = new_filename + ' ' + string
-        print(new_filename)
+        os.rename(filename, new_filename)
